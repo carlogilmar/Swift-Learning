f4182e89a05f9c12e11e291a26e4761df32d8a8f
carlogilmar <carlogilmar12@gmail.com>
2017-08-23 00:44:30 -0500

    Agregando primera prueba de swift

---
+++ learning.swift
@@ -0,0 +1,46 @@
+// Basic things about the language
+var message = "Hello, playground"
+print("Hello world")
+print(message)
+
+// Variables y constantes
+var myVariable = 100
+let myConstant = 111
+myVariable = 567
+
+// Tipados
+let explicitDouble: Double = 70
+let implicitDouble = 70.0
+let implicitInteger = 70
+let label:String = "Esto es un string tipado "
+let width = 98
+let widthLabel = label + String(width)
+print(widthLabel)
+
+//Interpolando en string
+let apples = 3
+let oranges = 10
+let appleSummary = "I have \(apples) apples"
+let fruitSummary = "I have \(apples + oranges) fruits"
+print(appleSummary)
+print(fruitSummary)
+
+//ArrayList
+var shoppingList = ["eggs", "milk", "potatos", "fruit", "icecream"]
+print(shoppingList[0])
+
+//Dictionary
+var dictionary = [
+    "Carlo":"Diez",
+    "Itzel":"Diez",
+    "Fer":"Nueve"
+]
+
+//Control Flow
+//Iteraciones
+let scores = [3,3,2,2]
+for score in scores{
+    print(score)
+}
+
+
