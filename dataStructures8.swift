4fc7e283cacda96cffd2465e2e99f093525d33d7
carlogilmar <carlogilmar12@gmail.com>
2017-11-04 01:05:56 -0600

    Adding stack implementation good!

--- dataStructures8.swift
+++ dataStructures8.swift
@@ -1,18 +1,22 @@
-// Stack
+public struct ArrayStack<T> {
 
-public struct Stack<T> {
-  private var elements = [T]()
+  fileprivate var elements = [T]()
+
   public init() {}
 
-  public mutating func pop() -> T?{
+  public init<S : Sequence>(_ s: S) where S.Iterator.Element == T {
+    self.elements = Array(s.reversed())
+  }
+
+  mutating public func pop() -> T? {
     return self.elements.popLast()
   }
 
-  public mutating func push(element: T){
+  mutating public func push(element: T){
     self.elements.append(element)
   }
 
-  public func peek() -> T?{
+  public func peek() -> T? {
     return self.elements.last
   }
 
@@ -25,12 +29,28 @@ public struct Stack<T> {
   }
 }
 
-var myStack = Stack<Int>()
-myStack.push(element:1)
-myStack.push(element:2)
-myStack.push(element:3)
-myStack.push(element:4)
-myStack.push(element:5)
+// Protocols
+extension ArrayStack: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: T...) {
+        self.init(elements)
+    }
+}
+
+extension ArrayStack: CustomStringConvertible, CustomDebugStringConvertible {
+  public var description: String {
+    return self.elements.description
+  }
+  public var debugDescription: String {
+    return self.elements.debugDescription
+  }
+}
+
+var myStack = ArrayStack<Int>()
+myStack.push(element: 1)
+myStack.push(element: 2)
+myStack.push(element: 3)
+myStack.push(element: 4)
+myStack.push(element: 5)
 print(myStack)
 print("Stack size: \(myStack.count)")
 var x = myStack.pop()
@@ -39,39 +59,3 @@ x = myStack.pop()
 print(myStack)
 print("Stack size: \(myStack.count)")
 
-
-// Protocols
-
-extension Stack: CustomStringConvertible, CustomDebugStringConvertible {
-  public var description: String{
-    return self.elements.description
-  }
-  public var debugDescription: String{
-    return self.elements.debugDescription
-  }
-}
-
-extension ArrayStack: ExpressibleByArrayLiteral {
-  public init(arrayLiteral elements: T...) {
-    self.init(elements)
-  }
-}
-
-public struct ArrayIterator<T> : IteratorProtocol {
-  var currentElements: [T]
-  init(elements: [T]){
-    self.currentElement = elements
-  }
-  mutating public func next() -> T?{
-    if(!self.currentElement.isEmpty){
-      return self.currentElement.popLast()
-    }
-    return nil
-  }
-}
-
-extension Stack: Sequence{
-  public func makeIterator() -> ArrayIterator<T> {
-    return ArrayIterator<T>(elements:self.elements)
-  }
-}
