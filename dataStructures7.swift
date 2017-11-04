c2070073c96f7c50775d7acb838a8ffb8091c03b
carlogilmar <carlogilmar12@gmail.com>
2017-11-04 00:02:16 -0600

    Adding protocol example

---
+++ dataStructures7.swift
@@ -0,0 +1,12 @@
+// Protocols
+
+protocol Particle{
+  var name:String { get }
+    func particalAsImage -> NSImage
+}
+
+/*
+  An iterator is any type that conforms to the iterator protocol
+*/
+
+
