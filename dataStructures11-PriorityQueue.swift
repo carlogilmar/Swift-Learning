2ec2ac15b05c28d1130285af0501d9f8da1727f9
carlogilmar <carlogilmar12@gmail.com>
2017-11-07 23:03:39 -0600

    Adding improvement

--- dataStructures11-PriorityQueue.swift
+++ dataStructures11-PriorityQueue.swift
@@ -1,3 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
 /*
  Prioriy Queue
  */
@@ -26,14 +32,15 @@ public struct PriorityQueue<T:Comparable>{
 
     public mutating func push (_ element: T){
         heap.append(element)
-        swin(heap.count - 1)
+        swim(heap.count - 1)
     }
+
 
     public mutating func pop() -> T? {
         if heap.isEmpty { return nil }
         if heap.count == 1 { return heap.removeFirst() }
 
-        swap(&heap[0], &heap[heap.count - 1])
+        //swap(&heap[0], &heap[heap.count - 1])
         let temp = heap.removeLast()
         sink(0)
         return temp
@@ -41,13 +48,56 @@ public struct PriorityQueue<T:Comparable>{
 
     public mutating func remove(_ item: T){
         if let index = heap.index(of: item) {
-            swap(&heap[index], &heap[heap.count - 1])
+            //swap(&heap[index], &heap[heap.count - 1])
             heap.removeLast()
-            swing(index)
+            swim(index)
             sink(index)
         }
     }
 
+    public mutating func removeAll (_ item: T){
+        var lastCount = heap.count
+        remove(item)
+        while(heap.count < lastCount){
+            lastCount = heap.count
+            remove(item)
+        }
+    }
+
+    public func peek() -> T?{
+        return heap.first
+    }
+
+    public mutating func clear(){
+        #if swift(>=3.0)
+            heap.removeAll(keepingCapacity: false)
+        #else
+            heap.removeAll(keepCapacity: false)
+        #endif
+    }
+
+    private mutating func sink(_ index:Int) {
+        var index = index
+        while 2 * index + 1 < heap.count {
+            var j = 2 * index + 1
+            if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j += 1 }
+            if !ordered(heap[index], heap[j]) { break }
+            //swap(&heap[index], &heap[j])
+            index = j
+        }
+    }
+
+    private mutating func swim(_ index: Int) {
+        var index = index
+        while index > 0 && ordered(heap[(index - 1) / 2],
+                                   heap[index]) {
+                                    //swap(&heap[(index - 1) / 2], &heap[index])
+                                    index = (index - 1) / 2
+        }
+    }
 
 }
+
+var priorityQueue = PriorityQueue<String>(ascending: true)
+print(priorityQueue)
 
