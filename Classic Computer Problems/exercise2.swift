73860b7317e6585cf310d2e3612afdf6988972f8
Carlo Gilmar <carlogilmar12@gmail.com>
2017-11-26 23:53:04 -0600

    Adding exercise for calculate pi

---
+++ Classic Computer Problems/exercise2.swift
@@ -0,0 +1,18 @@
+print("Calculando Pi")
+
+func calculatePi(nTerms:UInt)->Double{
+  let numerator: Double = 4
+  var denominator: Double = 1
+  var operation: Double = -1
+  var pi: Double = 0
+
+  for _ in 0..<nTerms{
+    pi += operation * (numerator/denominator)
+    denominator += 2
+    operation *= -1
+  }
+
+  return abs(pi)
+}
+
+print(calculatePi(nTerms:5))
