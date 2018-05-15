7a38ea0e830539047c431dbb81763fbf47830964
carlogilmar <carlogilmar12@gmail.com>
2018-05-14 23:27:28 -0500

    Adding my first protocols to Queue Struct

--- swift-basics/queue.swift
+++ swift-basics/queue.swift
@@ -1,13 +1,54 @@
-struct CoolQueue<C>{
+struct CoolQueue<T>{
 
-  var queue:[C]=[]
+  var queue:[T]=[]
 
-  mutating func enqueue(element:C){
+  mutating func enqueue(element:T){
     self.queue.append(element)
   }
 
-  mutating func dequeue() -> C?{
+  mutating func dequeue() -> T?{
     return self.queue.removeFirst()
   }
 
+  func peek() -> T?{
+    return self.queue.first
+  }
+
+  //Check this!
+  //public var capacity:Int{
+  //  get{
+  //    return queue.capacity
+  //  }
+  //  set{
+  //    return queue.reserveCapacity(newCapacity)
+  //  }
+  //}
+
 }
+
+
+// Protocols!
+
+extension CoolQueue: CustomStringConvertible, CustomDebugStringConvertible{
+
+  public var description: String{
+    return queue.description
+  }
+
+  public var debugDescription: String{
+    return queue.debugDescription
+  }
+
+}
+
+
+var queue = CoolQueue<Int>()
+queue.enqueue(element:1)
+queue.enqueue(element:2)
+queue.enqueue(element:3)
+queue.enqueue(element:4)
+queue.enqueue(element:5)
+
+print("========================")
+print( queue.debugDescription )
+print("========================")
