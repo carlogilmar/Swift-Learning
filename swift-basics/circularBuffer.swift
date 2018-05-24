69eec66206d4af7b824fc0617b7af23213dd42ed
carlogilmar <carlogilmar12@gmail.com>
2018-05-24 08:25:38 -0500

    Adding good example

--- swift-basics/circularBuffer.swift
+++ swift-basics/circularBuffer.swift
@@ -1,44 +1,40 @@
 struct CircularBuffer{
 
-    /*
-     push, pop, peek, isEmpty, isFull, clear
-     */
-    var storage:[Int]=[0,0,0]
+    var storage:[Int]=[0,0,0,0]
+    var maxSize:Int=4
     var index:Int=0
-    let maxSize:Int=3
 
     mutating func push(element:Int){
-
-        print("Current Index::\(index)::")
-
-        if index <= maxSize {
-            print(">> \(element) Insertando en index \(index)")
-            storage[index] = element
-            index = index + 1
-            print("Storage: \(self.storage)")
-        } else {
-            if index == maxSize+1{
-                storage[0] = element
-                index = 1
-                print("-- \(element) Insertando en index 0")
-                print("Storage: \(self.storage)")
-
+        if index < maxSize {
+            if(storage.count<maxSize){
+                print(" Adding \(element) at \(index)")
+                self.storage.insert(element, at:index)
+                index = index+1
+            } else {
+                print(" Updating \(element) at \(index)")
+                self.storage[index] = element
+                index = index+1
             }
+        } else {
+            print("Reset \(element) at 0")
+            self.storage[0] = element
+            index = 1
         }
-
     }
-
 }
 
 var buffer = CircularBuffer()
-buffer.index
+buffer.push(element: 10)
+buffer.push(element: 20)
+buffer.push(element: 30)
+buffer.push(element: 40)
+buffer.push(element: 50)
+buffer.push(element: 60)
+buffer.push(element: 70)
+buffer.push(element: 80)
+buffer.push(element: 90)
+buffer.push(element: 100)
+buffer.push(element: 110)
+buffer.push(element: 120)
+buffer.push(element: 130)
 buffer.storage
-buffer.push(element: 0)
-buffer.push(element: 1)
-buffer.push(element: 2)
-buffer.push(element: 3)
-buffer.push(element: 4)
-buffer.push(element: 5)
-buffer.push(element: 6)
-buffer.storage
-
