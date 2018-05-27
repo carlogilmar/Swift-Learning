4180b0f2be25319b4dc30e950b3693dc478d877c
carlogilmar <carlogilmar12@gmail.com>
2018-05-27 14:07:39 -0500

    Adding complete playground example drawing a circle

--- funAlgorithms/circle.swift
+++ funAlgorithms/circle.swift
@@ -1,5 +1,3 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
 class CircleView: UIView{
@@ -7,52 +5,31 @@ class CircleView: UIView{
     override func draw(_ rect: CGRect){
         let path = UIBezierPath()
         path.move(to: .zero)
+
         // Circle: x2 + y2 = r2
         let radious:Float = 100
+        let center = CGPoint(x: rect.width/2, y: rect.height/2)
 
-        for i in stride(from:0, to:300, by:10){
+        path.move(to: CGPoint(x: center.x + CGFloat(radious), y: center.y))
+
+        for i in stride(from:0, to:360, by:10){
             //print(i)
             //path.addLine(to: CGPoint(x:i, y:i))
-            let x = radious * cos(Float(i))
-            let y = radious * sin(Float(i))
+
+            let radians = Double(i) * Double.pi / 100
+
+            let x = Float(center.x) + radious * Float(cos(radians))
+            let y = Float(center.y) + radious * Float(sin(radians))
+
             // Fucking cast types are not dinamyc!!!
             path.addLine(to: CGPoint(x:Int(x), y:Int(y)))
 
-        }
-        //path.addLine(to: CGPoint(x:100, y:100))
-
-
-        path.stroke()
-    }
-
-}
-
-let view = CircleView(frame: CGRect(x:0, y:0, width:300, height:300))
-view.backgroundColor = .white
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-class CircleView: UIView{
-
-    override func draw(_ rect: CGRect){
-        let path = UIBezierPath()
-        path.move(to: .zero)
-        // Circle: x2 + y2 = r2
-        let radious:Float = 100
-
-        for i in stride(from:0, to:300, by:10){
-            //print(i)
-            //path.addLine(to: CGPoint(x:i, y:i))
-            let x = radious * cos(Float(i))
-            let y = radious * sin(Float(i))
-            // Fucking cast types are not dinamyc!!!
-            path.addLine(to: CGPoint(x:Int(x), y:Int(y)))
+            print("Drawing line (\(x), \(y))")
 
         }
         //path.addLine(to: CGPoint(x:100, y:100))
-
-
+        UIColor.green.setStroke()
+        path.lineWidth=5
         path.stroke()
     }
 
