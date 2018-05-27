058eca493c7fe1f94af01ee5cedb986415c01d77
carlogilmar <carlogilmar12@gmail.com>
2018-05-27 15:16:07 -0500

    Adding simple example using words as strings

---
+++ funAlgorithms/reverse.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var sample = "Hola a todos mis amigos que están aprendiendo a hacer swift y que tienen una macbook pro con batería muy pobre"
+
+let words = sample.components(separatedBy: " ")
+
+var w = "carlo"
+
+let wr = String(w.reversed())
+
+for c in w.characters { print(c) }
+
+/*
+If I don't import UIKit I couldn't use protocols as componets and reversed
+
+*/
