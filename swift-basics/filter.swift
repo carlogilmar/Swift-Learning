e930817d2ab4b3a95000740aace877698fd0ddcd
carlogilmar <carlogilmar12@gmail.com>
2018-05-27 01:27:35 -0500

    Adding example with structs

---
+++ swift-basics/filter.swift
@@ -0,0 +1,14 @@
+
+
+struct Student{
+  var name:String
+  var age:Int
+  var school:String
+}
+
+var carlo = Student(name:"carlo", age:23, school:"upiicsa")
+var luis = Student(name:"carlo1", age:17, school:"esime")
+var brady = Student(name:"carlo2", age:15, school:"esime")
+var fresi = Student(name:"carlo3", age:13, school:"upiicsa")
+var fer = Student(name:"carlo4", age:21, school:"esime")
+var malum = Student(name:"carlo5", age:25, school:"upiicsa")
