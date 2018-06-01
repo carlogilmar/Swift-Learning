3710cfc3995750c19a672fdb6432bc676f2eeebd
carlogilmar <carlogilmar12@gmail.com>
2018-05-31 23:20:51 -0500

    Adding function for find in the tree

--- funAlgorithms/binaryTree.swift
+++ funAlgorithms/binaryTree.swift
@@ -29,5 +29,21 @@ let fourteen = Node(value: 14, leftChild: eleven, rightChild: twelve)
 
 let ten = Node(value: 10, leftChild: five, rightChild: fourteen)
 
+func search(node: Node?, searchValue: Int) -> Bool {
 
+    if node == nil { return false }
 
+    if node?.value == searchValue {
+        return true
+    } else {
+        return search(node: node?.leftChild, searchValue: searchValue) || search(node: node?.rightChild, searchValue: searchValue)
+    }
+
+}
+
+print("Is there One? \( search(node: ten, searchValue: 1 ))")
+print("Is there eleven? \( search(node: ten, searchValue: 11 ))")
+print("Is there twenty? \( search(node: ten, searchValue: 20 ))")
+print("Is there fourteen? \( search(node: ten, searchValue: 14 ))")
+print("Is there one hundred? \( search(node: ten, searchValue: 100 ))")
+print("Is there six? \( search(node: ten, searchValue: 6 ))")
